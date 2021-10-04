282866bdfe5e6942b95904af0cfe682246051f44
QusayAL-Sherbi <qusay.alsherbi@gmail.com>
2021-10-04 09:50:27 +0200

    Creating Qusay AL-Sherbi Website

---
+++ js/master.js
@@ -0,0 +1,30 @@
+// Select Skills Selectors
+let ourSkills = document.querySelector(".skills-section");
+
+window.onscroll = function () {
+  
+  // Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop,
+
+      // Skills Outer Height
+      skillsOuterHeight = ourSkills.offsetHeight,
+
+      // Window Height
+      windowHeight = this.outerHeight;
+
+  // Window Scroll Top
+  let windowScrollTop = this.pageYOffset;
+
+  if (windowScrollTop >= skillsOffsetTop) {
+
+    let allSkills = document.querySelectorAll(".skill-box .skill-progress span");
+
+    allSkills.forEach(skill => {
+
+      skill.style.width = skill.dataset.progress;
+
+    });
+
+  }
+
+};
