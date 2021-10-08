61c3cd6bc908d1a3c8f7c96a08c44d26b0900daf
QusayAL-Sherbi <qusay.alsherbi@gmail.com>
2021-10-08 08:19:20 +0200

    Show Intro Text In Responsive, Add Skills Logo, Redesign Testimonials Section, Organize project images into folders

--- js/master.js
+++ js/master.js
@@ -2,29 +2,21 @@
 let ourSkills = document.querySelector(".skills-section");
 
 window.onscroll = function () {
-  
   // Skills Offset Top
   let skillsOffsetTop = ourSkills.offsetTop,
-
-      // Skills Outer Height
-      skillsOuterHeight = ourSkills.offsetHeight,
-
-      // Window Height
-      windowHeight = this.outerHeight;
+    // Skills Outer Height
+    skillsOuterHeight = ourSkills.offsetHeight,
+    // Window Height
+    windowHeight = this.outerHeight;
 
   // Window Scroll Top
-  let windowScrollTop = this.pageYOffset;
+  let windowScrollTop = this.pageYOffset + 100;
 
   if (windowScrollTop >= skillsOffsetTop) {
+    let allSkills = document.querySelectorAll(
+      ".skill-box .skill-progress span"
+    );
 
-    let allSkills = document.querySelectorAll(".skill-box .skill-progress span");
-
-    allSkills.forEach(skill => {
-
-      skill.style.width = skill.dataset.progress;
-
-    });
-
+    allSkills.forEach((skill) => (skill.style.width = skill.dataset.progress));
   }
-
 };
